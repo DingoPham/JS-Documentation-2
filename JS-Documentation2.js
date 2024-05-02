14bcece0e3305081db8925ff8e73d6198fb53806
DingoPham <pencibambo@gmail.com>
2024-05-02 10:02:14 +0700

    export module

--- JS-Documentation2.js
+++ JS-Documentation2.js
@@ -1,5 +1,4 @@
-/* Có thể tìm danh mục bài tập và danh mục phần bành cách nhấn tổ hợp Ctrl + F và gõ BT/Phần + số danh mục muốn xem */
-/* !!! Sử dụng môi trưởng Console của trình duyệt Website hoặc node.js để làm môi trường chạy các ví dụ bên dưới !!! */
+/* Có thể tìm danh mục bài tập và danh mục phần bành cách nhấn tổ hợp Ctrl + F và gõ Phần + số danh phần muốn xem */
 
 /*-------------------------------- Phần 1: Hiểu về ECMAScript(ES) ---------------------------------*/
 //  a) ECMAScript
@@ -302,16 +301,398 @@
                 }
     // Xử lý bất đồng bộ: Sử dụng Callback, Promise hay async, await?
         /**
-         * Không nên sử dụng Callback do có thể xảy ra `callback hell`.
-         * Để trình bày code flow thì async/await sẽ thuận tiện hơn .then nhưng sẽ khó debug hơn mặc dù
-        Devtool đã hỗ trợ cú pháp async/await.
-        => Cân nhắc sử dụng cú pháp async/await và Promise tuỳ từng trường hợp sao cho hợp lý. Hạn chế 
-        tối đa việc sử dụng callback
+             * Không nên sử dụng Callback do có thể xảy ra `callback hell`.
+             * Để trình bày code flow thì async/await sẽ thuận tiện hơn .then nhưng sẽ khó debug hơn mặc dù
+            Devtool đã hỗ trợ cú pháp async/await.
+            => Cân nhắc sử dụng cú pháp async/await và Promise tuỳ từng trường hợp sao cho hợp lý. Hạn chế 
+            tối đa việc sử dụng callback
         */
-
 /*-------------------------------- Phần 3: Hướng đối tượng trong JavaScript ---------------------------------*/
+    // Class
+    /* JavaScript không được thiết kế đặc biệt cho lập trình hướng đối tượng, nhưng nó vẫn hỗ trợ các khái niệm
+    cơ bản của lập trình hướng đối tượng.*/
+        // Sử dụng function để tạo class
+            // Định nghĩa một lớp sử dụng hàm tạo
+            function Animal(name, sound){
+                this.name = name;
+                this.sound = sound;
+            }
+            // Thêm một phương thức vào vòng lặp
+            Animal.prototype.makeSound = function(){
+                console.log(this.name + ' make a ' + this.sound + ' sound.');
+            };
+            // Tạo đối tượng từ class Animal
+            var cat = new Animal('Cat', 'Meow');
+            var dog = new Animal('Dog', 'Bark');
+            // Gọi phương thức trến đối tượng
+            cat.makeSound();
+            dog.makeSound();
+        // Tạo Class bằng từ khoá Class ES6
+            // Định nghĩa một lớp (Class) theo cách hiện đại 
+            class Animal{
+                constructor(name, sound){
+                    this.name = name;
+                    this.sound = sound;
+                }
 
+                makeSound(){
+                    console.log(this.name + ' make a ' + this.sound + ' sound.');
+                }
+            }
+            var cat = new Animal('Cat', 'Meow');
+            var dog = new Animal('Dog', 'Bark');
+
+            cat.makeSound();
+            dog.makeSound();    
+        // So sánh các cách định nghĩa lớp
+            // Sử dụng Function
+                /** Đặc điểm
+                     * Cú pháp: Sử dụng function constructor
+                     * Tạo đối tượng mới: Sử dụng từ khoá new
+                     * Các thuộc tính: Được khởi tạo trong hàm constructor
+                     * Các phương thức: Thêm vào protype của hàm constructor
+                     * Kế thừa: không hỗ trợ trực tiếp, có thể sử dụng Object.create() hoặc Object.assign() 
+                     * Sử dụng 'this': Sử dụng 'this' trong phạm vi đối tượng
+                     * Tính đa dạng: Có thể tạo ra nhiều hàm constructor khác nhau
+                     * Tính linh hoạt: Tạo ra cấu trúc đối tượng linh hoạt và tuỳ chỉnh
+                     * Tính đồng nhất: Các đối tượng có thể có cấu trúc và phương thức khác nhau
+                */
+            // Sử dụng Class
+                /** Đặc điểm
+                     * Cú pháp: Sử dụng từ khóa class
+                     * Tạo đối tượng mới: Sử dụng từ khóa ne
+                     * Các thuộc tính: Được khởi tạo trong phương thức constructor (method) có tên là constructor
+                     * Các phương thức: Định nghĩa bên trong class
+                     * Kế thừa: Hỗ trợ kế thừa bằng từ khóa extends
+                     * Sử dụng 'this': Sử dụng 'this' trong phạm vi đối tượng
+                     * Tính đa dạng: Một class chỉ tạo ra một loại đối tượng
+                     * Tính linh hoạt: Giới hạn bởi cấu trúc class và kế thừa
+                     * Tính đồng nhất: Các đối tượng có cùng cấu trúc và phương thức
+                */
+        // Định nghĩa lớp sử dụng constructor function
+            /**
+             * Ưu điểm:
+                * Linh hoạt: Dễ dàng tạo ra và sử dụng các hàm độc lập.
+                * Thích hợp cho tác vụ nhỏ: Phù hợp cho các tác vụ đơn giản và nhỏ.
+            * Nhược điểm:
+                * Khó quản lý với số lượng hàm lớn: Khó quản lý khi có nhiều hàm liên quan.
+                * Khó tái sử dụng: Việc tái sử dụng code không dễ dàng như class
+            * Khuyến nghị: Sử dụng cho các tác vụ đơn giản, nhỏ, không cần sự liên kết giữa các chức năng 
+            */
+        // Định nghĩa lớp sử dụng `class`
+            /**
+             * Ưu điểm:
+                * Tính tổ chức cao: Class cho phép bạn tổ chức code một cách gọn gàng và có cấu trúc.
+                * Tính đóng gói và tái sử dụng cao: Các phương thức và thuộc tính được tổ chức gọn gàng 
+                trong một lớp, dễ dàng quản lý và tái sử dụng.
+                * Tính kế thừa tốt: Có thể tận dụng tính kế thừa để mở rộng và sửa đổi hành vi của lớp.
+                * Tính bảo trì cao: Khi dự án phát triển, việc thêm, sửa đổi hoặc loại bỏ các tính năng trở nên dễ 
+                dàng hơn với class. Các thay đổi chỉ cần thực hiện ở một nơi và có thể ảnh hưởng đến toàn bộ 
+                dự án
+             * Nhược điểm:
+                * Chỉ có từ ES6
+                * Có thể quá phức tạp cho các tác vụ nhỏ: Không phù hợp cho các tác vụ đơn giản
+             * Khuyến nghị: Sử dụng cho các tác vụ lớn, phức tạp, đòi hỏi tính chất đóng gói và tái sử dụng cao.
+             Ở MISA thì đều là các dự án lớn, phức tạp và cần những ưu điểm của class.
+            */
+        // Tính kế thừa
+            /** 
+                 * Các class con được kế thừa các thuộc tính và phương thức của lớp cha. 
+                 Sử dụng từ khóa extends để thực hiện kế thừa
+                 * Sử dụng từ khóa super() để gọi lên phương thức hoặc thuộc tính của lớp cha
+            */
+            // VD: constructor function
+            import BaseList from "../base/baselist.js";
+
+            class CustomerPage extends BaseList{
+                constructor(){
+                    super("Quản lý sản phẩm","https://cukcuk.vn/api/v1/customers");
+                }
+                // Override lại phương thức của lớp cha
+                initEvents(){
+                    //add new events:
+                    const btnAdd = document.getElementById("btnAdd");
+                    btnAdd.addEventListener("click", this.onAdd);
+                }
+                // Xử lý khi ấn Thêm mới...
+                onAdd(){
+                    //...
+                }
+            }
+            export default new CustomerPage();
+            // VD: class
+            class BaseList2{
+                constructor(titlePage, api){
+                    this._titlePage = titlePage;
+                    this.apiUrl = api;
+                    this.initEvents();
+                    this.fetchData();
+                }
+                // Khởi tạo Page:
+                initPage(){
+                    //...
+                }
+                // Hàm tạo cho các Event cho Page:
+                initEvents(){
+                    //logic code...
+                }
+                // Lấy dữ liệu từ Api
+                fetchData(){
+                    //...
+                }
+                setTableData(data){
+                    //...
+                }
+            }
+            export default BaseList2;
+        // Tính đa hình
+            /**
+             * Lớp con có thể ghi đè(override) thuộc tính hoặc phương thức của lớp cha
+             * JS không hỗ trợ overload(nạp chồng)
+            */
+           // VD: constructor function
+           import BaseList from "../base/baselist.js";
+
+           class CustomerPage extends BaseList{
+               constructor(){
+                   super("Quản lý sản phẩm","https://cukcuk.vn/api/v1/customers");
+               }
+               // Override lại phương thức của lớp cha
+               initEvents(){ // ghi đè lớp cha
+                   //add new events:
+                   const btnAdd = document.getElementById("btnAdd");
+                   btnAdd.addEventListener("click", this.onAdd);
+               }
+               // Xử lý khi ấn Thêm mới...
+               onAdd(){
+                   //...
+               }
+           }
+           export default new CustomerPage();
+           // VD: class
+           class BaseList2{
+               constructor(titlePage, api){
+                   this._titlePage = titlePage;
+                   this.apiUrl = api;
+                   this.initEvents();
+                   this.fetchData();
+               }
+               // Khởi tạo Page:
+               initPage(){
+                   //...
+               }
+               // Hàm tạo cho các Event cho Page:
+               initEvents(){ // Ghi đè lớp cha
+                   //logic code...
+               }
+               // Lấy dữ liệu từ Api
+               fetchData(){
+                   //...
+               }
+               setTableData(data){
+                   //...
+               }
+           }
+           export default BaseList2;
+           // VD: Tính đa hình - JS không hỗ trợ nạp chồng (overload)
+           class MsgBox{
+                showMessengerBox(){
+                    return "MessengerBox 1";
+                }
+                showMessengerBox(messager){
+                    return "MessengerBox 2";
+                }
+           }
+           const msgBox = new MsgBox();
+
+           // Khi gọi thì sẽ chỉ thực thi hàm viết sau cùng:
+           msgBox.showMessengerBox(); // KQ là "MessengerBox 2"
+           msgBox.showMessengerBox("mmmmm"); // KQ là "MessengerBox 2"
+        // Tính trừu tượng
+           /* Trong JavaScript, không hỗ trợ trực tiếp cho lớp trừu tượng, tuy nhiên bạn có thể mô phỏng tính 
+           chất trừu tượng bằng cách sử dụng các phương thức trừu tượng trong các lớp con 
+           */
+           // VD: cho lớp trừu tượng Animal
+            class Animal{
+                constructor(name){
+                    this.name = name;
+                }
+                // Phương thức trừu tượng
+                makeSound(){
+                    throw new Error("Phương thức makeSound phải được triển khai");
+                }
+            }
+
+            // Lớp con kế thừa từ lớp trừu tượng Animal
+            class Dog extends Animal{
+                constructor(name){
+                    super(name);
+                }
+                makeSound(){
+                    return "Woof!";
+                }
+            }
+            
+            // Tạo đối tượng từ lớp con
+            const myDog = new Dog("Buddy");
+
+            // Gọi phương thức của lớp con
+            console.log(myDog.makeSound()); //KQ sẽ là âm thanh của chó => "Woof!"
+
+            // Gọi phương thức trừu tượng từ lớp cha
+            const myAnimal = new Animal("Generic");
+            try{
+                console.log(myAnimal.makeSound());
+            } catch (error){
+                console.log(error.message); // KQ là thông báo lỗi từ phương thức trừu tượng
+            }
+        // Tính đóng gói
+            /** 
+             * Trong JavaScript, mặc dù không hỗ trợ trực tiếp cho việc thiết lập kiểu truy cập riêng tư (private) hoặc công khai (public) cho thuộc tính và 
+            phương thức, nhưng bạn có thể sử dụng các quy ước mô phỏng tính chất đóng gói.
+             * JavaScript không hỗ trợ phạm vi protected
+            */
+            // VD mô phỏng tính đóng gói trong class
+            class Counter{
+                #count = 0; // Thuộc tính riêng tư (private)
+
+                #increaseCount(){ // private
+                    this.#count++;
+                }
+                getCount(){ // public
+                    return this.#count; // Phương thức công khai
+                }
+                increment(){ // public
+                    this.#increaseCount(); // Gọi phương thức riêng tư từ bên trong Class
+                }
+            }
+            // Sử dụng đối tượng Counter
+            const counter = new Counter();
+            counter.increment();
+            console.log(counter.getCount()); 
+        // Thành phần tĩnh
+            /* Có thể khai báo các thành phần (thuộc tính, phương thức) tĩnh trong class 
+            bằng từ khóa static */
+            // Sử dụng thành phần tĩnh trong class
+            class MathHelper{
+                static muliply(x, y){
+                    return x * y; // Phương thức tĩnh
+                }
+
+                static square(x){
+                    return x * y; // Phương thức tĩnh
+                }
+            }
+
+            // Sử dụng phương thức tĩnh từ class
+            console.log(MathHelper.muliply(5, 3)); // KQ = 15
+            console.log(MathHelper.square(4)); // KQ = 16
+        // Prototype-Based OOP
+            /*Mô hình lập trình dựa trên nguyên mẫu (Prototype-Based OOP) cho phép bạn
+            tạo các đối tượng bằng cách sử dụngn guyên mẫu và kế thừa tính chất từ nguyên mẫu đó */
+            // Định nghĩa đối tượng nguyên mẫu
+            let animal = {
+                type: 'Unknow',
+                displayType: function(){
+                    return 'Type: ' + this.type;
+                }
+            };
+            // Tạo một đối tượng mới kế thừa từ nguyên mẫu animal
+            let dog = Object.create(animal);
+            dog.type = 'Dog';
+            // Tạo một đối tượng mới kế thừa từ nguyên mẫu animal
+            let cat = Object.create(animal);
+            cat.type = 'Cat';
+            // In ra thông tin về đối tượng dog và cat
+            console.log(dog.displayType());
+            console.log(cat.displayType());
+        // Mixin
+            /* Sử dụng mixin để kết hợp các tính chất từ nhiều đối tượng thành một đối tượng mới */
+            // Định nghĩa các mixin
+            let sayNameMixin = {
+                sayName(){
+                    console.log("My name is " + this.name);
+                }
+            };
+            let sayAgeMinxin = {
+                sayAge(){
+                    console.log("I am " + this.age + " years old");
+                }
+            };
+            // Định nghĩa một đối tượng 
+            let person = {
+                name: 'Joe Doe',
+                age: 20
+            };
+            // Kết hợp các mixin vào đối tượng person
+            Object.assign(person, sayNameMixin, sayAgeMinxin);
+            // Gọi các phương thức từ Mixin
+            person.sayName();
+            person.sayAge();
 /*-------------------------------- Phần 4: Export Module ---------------------------------*/
+    /**
+     * Là một cơ chế cho phép xuất các biến , hàm, lớp hoặc đối tượng từ một file (module) để có thể
+    sử dụng trong các file (module) khác.
+     *  Cơ chế này giúp tổ chức và tái sử dụng mã nguồn hiệu quả trong các dự án lớn hoặc phân chia
+    thành các phần nhỏ hơn để dễ quản lý.
+    */
+    // Default Export
+        /* Default Export: chỉ định một giá trị hoặc một đối tượng để xuất ra khỏi module. Mỗi Module
+        chỉ có thể có một default export.*/
+        class MISATable extends HTMLElement{    // table.js
+            constructor(){
+                super();
+                const api = this.getAttribute("api");
+                this._apiUrl = api;
+                this._columns = [];
+                this._apiUrl = api;
+                this._actionElements = [];
+                // Tạo một shadow root 
+                this.attachShadow({ mode: "open"});
+                // set HTML & CSS:
+                this.shadowRoot.innerHTML = `<div style="display: none">
+                                             <slot name: "toolbar"></slot>
+                                             </div>`;
+                this.removeAttribute("api");
+            }
+            //#region Methods
+        }
+        export default MISATable;
+
+        import MISATable from '.Components/table/table.js'; // component.js
+        customElements.define("m-table", MISATable);        
+    // Named Export
+        /* Named Export: xuất nhiều biến, hàm, lớp hoặc đối tượng bằng cách đặt tên cho mỗi mục muốn
+        xuất. Có thể có nhiều named exports trong một module */
+        // common.js
+        export function formDate(dateValue){
+            try{
+                const dateTimeValue = new Date(dateValue);
+                let date = dateTimeValue.getDate();
+                let month = dateTimeValue.getMonth() +1;
+                let year = dateTimeValue.getFullYear();
+                return `${date}/${month}/${year}`;
+            }
+            catch (error){
+                console.error(error);
+                return "";
+            }
+        }
+        // basepage.js
+        import BaseList from '../base/baselist.js';
+        import { formDate } from '../js/base/common.js';
+        class BasePage extends BaseList{
+            constructor(){
+                super();
+                this.fillData();
+            }
+            fillData(){
+                let newDate = new Date();
+                const dateFormatString = formDate(newDate);
+                console.log(dateFormatString);
+                // ...
+            }
+        }
 
 /*-------------------------------- Phần 5: Web Component ---------------------------------*/
 
